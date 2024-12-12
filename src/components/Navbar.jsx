dabddf032ce5b6c71e303721e194c0b46fa69208
abhijeetnagpure08 <abhijeetnagpureofficial@gmail.com>
2024-12-12 11:35:45 +0530

    Profile, Logout functionality created and updated using RTK query

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Menu, School } from "lucide-react";
-import React from "react";
+import React, { useEffect } from "react";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -31,10 +31,26 @@ import {
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { Separator } from "@radix-ui/react-dropdown-menu";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useLogoutUserMutation } from "@/features/api/authApi";
+import { toast } from "sonner";
+import { useSelector } from "react-redux";
 
 export const Navbar = () => {
-  const user = true;
+  const { user } = useSelector((store) => store.auth);
+  const navigate = useNavigate();
+  const [logoutUser, { data, isSuccess }] = useLogoutUserMutation();
+
+  const logoutHandler = async () => {
+    await logoutUser();
+  };
+
+  useEffect(() => {
+    if (isSuccess) {
+      toast.success(data.message || "Logout successfull");
+      navigate("/login");
+    }
+  }, [isSuccess]);
   return (
     <div className="h-16 dark:bg-[#0A0A0A] bg-white border-b dark:border-b-gray-800 border-b-gray-200 fixed top-0 left-0 right-0 duration-300 z-10">
       {/*Desktop  max-w-screen-2xl */}
@@ -51,7 +67,7 @@ export const Navbar = () => {
               <DropdownMenuTrigger asChild>
                 <Avatar>
                   <AvatarImage
-                    src="https://github.com/shadcn.png"
+                    src={user.photoUrl || "https://github.com/shadcn.png"}
                     alt="@shadcn"
                   />
                   <AvatarFallback>CN</AvatarFallback>
@@ -61,60 +77,74 @@ export const Navbar = () => {
                 <DropdownMenuLabel>My Account</DropdownMenuLabel>
                 <DropdownMenuSeparator />
                 <DropdownMenuGroup>
-                  <DropdownMenuItem><Link to="my-learning">My Learning</Link></DropdownMenuItem>
-                  <DropdownMenuItem><Link to="profile">Edit Profile</Link></DropdownMenuItem>
-                  <DropdownMenuItem>Log Out</DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Link to="my-learning">My Learning</Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Link to="profile">Edit Profile</Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem onClick={logoutHandler}>
+                    Log Out
+                  </DropdownMenuItem>
                 </DropdownMenuGroup>
-                <DropdownMenuSeparator />
-
-                <DropdownMenuItem>Dashboard</DropdownMenuItem>
+                {user.role === "instructor" && (
+                  <>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>Dashboard</DropdownMenuItem>
+                  </>
+                )}
               </DropdownMenuContent>
             </DropdownMenu>
           ) : (
             <div className="flex items-center gap-2">
-              <Button variant="outline">Login</Button>
-              <Button>Signup</Button>
+              <Button variant="outline" onClick={() => navigate("/login")}>
+                Login
+              </Button>
+              <Button onClick={() => navigate("/login")}>Signup</Button>
             </div>
           )}
           <DarkMode />
         </div>
       </div>
       <div className="md:hidden flex items-center justify-between px-4 h-full">
-      <h1 className="font-extrabold text-2xl">E-Learning</h1>
-      <MobileNavbar />
+        <h1 className="font-extrabold text-2xl">E-Learning</h1>
+        <MobileNavbar />
       </div>
     </div>
   );
 };
 
 const MobileNavbar = () => {
-    const role = "instructor"
+  const role = "instructor";
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <Button size="icon" className="rounded-full bg-gray-200 hover:bg-gray-200" variant="outline"><Menu/></Button>
+        <Button
+          size="icon"
+          className="rounded-full bg-gray-200 hover:bg-gray-200"
+          variant="outline"
+        >
+          <Menu />
+        </Button>
       </SheetTrigger>
       <SheetContent className="flex flex-col">
         <SheetHeader className="flex flex-row items-center justify-between mt-2">
           <SheetTitle>E-Learning</SheetTitle>
-          <DarkMode/>
+          <DarkMode />
         </SheetHeader>
-        <Separator className="mr-2"/>
-       <nav className="flex flex-col space-y-4">
-        <span>My Learning</span>
-        <span>Edit Profile</span>
-        <p>Log Out</p>
-       </nav>
-       {
-        role === "instructor" && (
-            <SheetFooter>
-          <SheetClose asChild>
-            <Button type="submit">Dashboard</Button>
-          </SheetClose>
-        </SheetFooter>
-        )
-       }
-        
+        <Separator className="mr-2" />
+        <nav className="flex flex-col space-y-4">
+          <span>My Learning</span>
+          <span>Edit Profile</span>
+          <p>Log Out</p>
+        </nav>
+        {role === "instructor" && (
+          <SheetFooter>
+            <SheetClose asChild>
+              <Button type="submit">Dashboard</Button>
+            </SheetClose>
+          </SheetFooter>
+        )}
       </SheetContent>
     </Sheet>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -68,10 +68,10 @@ export const Login = () => {
       toast.success(registerData.message || "Signup Successfull.");
     }
     if(registerError){
-      toast.error(registerData.data.message ||"Signup Failed")
+      toast.error(registerError.data.message ||"Signup Failed")
     }
     if(loginError){
-      toast.error(loginData.data.message ||"Signup Failed")
+      toast.error(loginError.data.message ||"Signup Failed")
     }
     if (loginIsSuccess && loginData) {
       toast.success(loginData.message || "Login Successfull.");
@@ -88,7 +88,7 @@ export const Login = () => {
 
   return (
     <div className="flex item-center w-full justify-center mt-20">
-      <Tabs defaultValue="account" className="w-[400px]">
+      <Tabs defaultValue="login" className="w-[400px]">
         <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger value="signup">Signup</TabsTrigger>
           <TabsTrigger value="login">Login</TabsTrigger>
